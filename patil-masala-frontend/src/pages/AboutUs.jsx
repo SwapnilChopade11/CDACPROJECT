629695ffb7aec9d9477c6b96896a52554c2f5e2a
Swapnil Chopade <swapnilchopade9@gmail.com>
2023-08-31 01:49:46 +0530

    Front End Folder

---
+++ patil-masala-frontend/src/pages/AboutUs.jsx
@@ -0,0 +1,20 @@
+
+
+const AboutUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <b>
+        <h3>Our Story</h3>
+        <br />
+        <br />
+        <p>We started this Spices venture in 2015 with Small, honest beginnings. Steady, sustained growth. A circle of ever-expanding, committed stakeholders and an ever-growing loyal customer base, making us today one of the most trusted brands across our District level.
+From a modest home-grown venture to a brand with cutting-edge production units, state-of- the-art machinery and path-breaking research and development tools, the  <b> PATIL MASALA </b>  journey is the stuff of not only stories, but of legend.
+</p>
+        <br />
+        <br /> 
+      </b>
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ patil-masala-frontend/src/pages/ContactUs.jsx
@@ -0,0 +1,22 @@
+import "./ContactUs.css";
+const ContactUs = () => {
+  return (
+    <div className="contact"><h1>Patil Masala</h1>
+      <b>
+        
+        <div className="column-4"><h1>Address:</h1><h>Near Gram Panchayat Fekari,<br></br>
+        Deepnagar,Bhusawal MH 425307</h></div>
+          <div className="column-4">
+          <h1><img src="" alt="" />E-mail:</h1><h5>dhirajap24@gmail.com</h5></div>
+          <div className="column-4"><h1>Phone No:</h1><h5>9975575082</h5></div>
+          
+      </b>
+      
+    </div>
+  );
+};
+<script>
+
+</script>
+
+export default ContactUs;
